096d5b376a381d3a203bfe95b87c5f12407fe135
AlexFanning14 <alexfanning14@gmail.com>
2017-07-23 17:16:48 +0100

    Nav drawer not working

--- app/src/main/java/com/alexfanning/artwaves/MainActivity.java
+++ app/src/main/java/com/alexfanning/artwaves/MainActivity.java
@@ -1,13 +1,27 @@
 package com.alexfanning.artwaves;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlanetTitles = getResources().getStringArray(R.array.drawer_fragment_items);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
     }
 }
