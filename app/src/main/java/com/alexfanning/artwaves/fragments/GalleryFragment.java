a564da4bd592468c91583ddce3b8519d329a68c9
alex.fanning <alexfanning14@gmail.com>
2017-08-24 11:36:21 +0100

    Image Gallery Recycler View Working

--- app/src/main/java/com/alexfanning/artwaves/fragments/GalleryFragment.java
+++ app/src/main/java/com/alexfanning/artwaves/fragments/GalleryFragment.java
@@ -5,20 +5,23 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alexfanning.artwaves.R;
 import com.alexfanning.artwaves.galleryitems.Gallery;
-import com.alexfanning.artwaves.galleryitems.GalleryPagerAdapter;
+import com.alexfanning.artwaves.galleryitems.GalleryDataAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
 **/
 public class GalleryFragment extends Fragment {
     private Context mContext;
-
+    private RecyclerView mGalleryRv;
+    private GalleryDataAdapter mGalAdapter;
     @Override
     public void onAttach(Context context) {
         mContext = context;
@@ -26,22 +29,26 @@ public class GalleryFragment extends Fragment {
 
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
-        Gallery[] items = new Gallery[]{
-                new Gallery("Picture Title",mContext.getDrawable(R.drawable.sample_img),"John Doe","Sample Descritpionj")
-        };
-
-        GalleryPagerAdapter galleryPagerAdapter = new GalleryPagerAdapter(mContext,items);
-
-        ViewPager mViewPager = (ViewPager)rootView.findViewById(R.id.pager);
-        mViewPager.setAdapter(galleryPagerAdapter);
-
+        Gallery[] items = generateGalleryArray();
+        mGalleryRv = (RecyclerView)rootView.findViewById(R.id.rv_galleries);
+        mGalleryRv.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false));
+        mGalleryRv.setHasFixedSize(true);
+        mGalAdapter = new GalleryDataAdapter(items,mContext);
+        mGalleryRv.setAdapter(mGalAdapter);
         return rootView;
     }
 
+
+    private Gallery[] generateGalleryArray(){
+        return new Gallery[]{
+                new Gallery("Picture Title",mContext.getDrawable(R.drawable.sample_img),"John Doe","Sample Descritpionj"),
+                new Gallery("Picture Title",mContext.getDrawable(R.drawable.sample_img2),"John Doe","Sample Descritpionj"),
+                new Gallery("Picture Title",mContext.getDrawable(R.drawable.sample_img3),"John Doe","Sample Descritpionj")
+        };
+    }
 
 
 }
--- app/src/main/java/com/alexfanning/artwaves/galleryitems/Gallery.java
+++ app/src/main/java/com/alexfanning/artwaves/galleryitems/Gallery.java
@@ -1,15 +1,14 @@
 package com.alexfanning.artwaves.galleryitems;
 
 import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by User on 22-Aug-17.
  */
 
-public class Gallery {
-
-
-
+public class Gallery implements Parcelable {
 
     private String title;
     private Drawable img;
@@ -39,7 +38,38 @@ public class Gallery {
         return descript;
     }
 
+    protected Gallery(Parcel in) {
+        title = in.readString();
+        img = (Drawable) in.readValue(Drawable.class.getClassLoader());
+        artist = in.readString();
+        descript = in.readString();
+    }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeValue(img);
+        dest.writeString(artist);
+        dest.writeString(descript);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Gallery> CREATOR = new Parcelable.Creator<Gallery>() {
+        @Override
+        public Gallery createFromParcel(Parcel in) {
+            return new Gallery(in);
+        }
+
+        @Override
+        public Gallery[] newArray(int size) {
+            return new Gallery[size];
+        }
+    };
 
 
 }
